a19739991eb0d53ce59bbb94bc8705bc239271ef
root <root@amambai.fiocruz.ms>
2016-07-21 06:37:41 -0400

    Adicionado Localidades estaduais

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `producao`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `producao` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `producao`;
-
---
 -- Table structure for table `armazem_armazem`
 --
 
@@ -165,7 +157,7 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$24000$VmyS8ArgTQXz$DD5BnKtIVagxTPsl0VL+TGM2u/3eRRuxD/Ef3RY3MCc=','2016-07-20 16:33:07',1,'admin','','','admin@matogrossodosul.fiocruz.br',1,1,'2016-07-20 16:30:56'),(2,'pbkdf2_sha256$24000$wshnIBVPgWHz$dZPcMJf4WXju5Ns+C6HZpxVPqEhCx8OAGU5QROq963U=','2016-07-20 16:34:10',0,'usuario','Usuário','Usuário','usuario@matogrossodosul.fiocruz.br',0,1,'2016-07-20 16:31:33'),(3,'pbkdf2_sha256$24000$mZ8kdn3yNKgj$zOITOSpaVGQOvuxtsVMCM2dTGEnlb8Olk9CsQZIL9g8=','2016-07-20 16:34:23',0,'master','Master','Master','master@matogrossodosul.fiocruz.br',0,1,'2016-07-20 16:31:56');
+INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$24000$VmyS8ArgTQXz$DD5BnKtIVagxTPsl0VL+TGM2u/3eRRuxD/Ef3RY3MCc=','2016-07-21 09:38:16',1,'admin','Administrador','Administrador','admin@matogrossodosul.fiocruz.br',1,1,'2016-07-20 16:30:56'),(2,'pbkdf2_sha256$24000$wshnIBVPgWHz$dZPcMJf4WXju5Ns+C6HZpxVPqEhCx8OAGU5QROq963U=','2016-07-20 16:34:10',0,'usuario','Usuário','Usuário','usuario@matogrossodosul.fiocruz.br',0,1,'2016-07-20 16:31:33'),(3,'pbkdf2_sha256$24000$mZ8kdn3yNKgj$zOITOSpaVGQOvuxtsVMCM2dTGEnlb8Olk9CsQZIL9g8=','2016-07-21 10:35:36',0,'master','Master','Master','master@matogrossodosul.fiocruz.br',0,1,'2016-07-20 16:31:56');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -240,7 +232,7 @@ CREATE TABLE `core_estado` (
   `criado` datetime NOT NULL,
   `atualizado` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -249,6 +241,7 @@ CREATE TABLE `core_estado` (
 
 LOCK TABLES `core_estado` WRITE;
 /*!40000 ALTER TABLE `core_estado` DISABLE KEYS */;
+INSERT INTO `core_estado` VALUES (1,'AC','Acre','2016-07-21 10:03:57','2016-07-21 10:03:57'),(2,'AL','Alagoas','2016-07-21 10:04:07','2016-07-21 10:04:07'),(3,'AP','Amapá','2016-07-21 10:04:19','2016-07-21 10:04:19'),(4,'AM','Amazonas','2016-07-21 10:04:30','2016-07-21 10:04:30'),(5,'BA','Bahia','2016-07-21 10:04:38','2016-07-21 10:04:38'),(6,'CE','Ceará','2016-07-21 10:05:05','2016-07-21 10:05:05'),(7,'DF','Distrito Federal','2016-07-21 10:05:15','2016-07-21 10:05:15'),(8,'GO','Goiás','2016-07-21 10:05:43','2016-07-21 10:05:43'),(9,'MA','Maranhão','2016-07-21 10:06:08','2016-07-21 10:06:08'),(10,'MT','Mato Grosso','2016-07-21 10:06:17','2016-07-21 10:06:17'),(11,'MS','Mato Grosso do Sul','2016-07-21 10:06:26','2016-07-21 10:06:26'),(12,'MG','Minas Gerais','2016-07-21 10:06:38','2016-07-21 10:06:38'),(13,'PA','Pará','2016-07-21 10:06:52','2016-07-21 10:06:52'),(14,'PB','Paraíba','2016-07-21 10:07:02','2016-07-21 10:07:02'),(15,'PR','Paraná','2016-07-21 10:07:12','2016-07-21 10:07:12'),(16,'PE','Pernambuco','2016-07-21 10:07:25','2016-07-21 10:07:25'),(17,'PI','Piauí','2016-07-21 10:07:40','2016-07-21 10:07:40'),(18,'RJ','Rio de Janeiro','2016-07-21 10:07:50','2016-07-21 10:07:50'),(19,'RN','Rio Grande do Norte','2016-07-21 10:08:10','2016-07-21 10:08:10'),(20,'RS','Rio Grande do Sul','2016-07-21 10:08:18','2016-07-21 10:08:18'),(21,'RO','Rondônia','2016-07-21 10:08:31','2016-07-21 10:10:38'),(22,'RR','Roraima','2016-07-21 10:08:38','2016-07-21 10:08:38'),(23,'SC','Santa Catarina','2016-07-21 10:08:53','2016-07-21 10:08:53'),(24,'SP','São Paulo','2016-07-21 10:09:03','2016-07-21 10:09:03'),(25,'SE','Sergipe','2016-07-21 10:09:12','2016-07-21 10:09:12'),(26,'TO','Tocantins','2016-07-21 10:09:23','2016-07-21 10:09:23');
 /*!40000 ALTER TABLE `core_estado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -268,7 +261,7 @@ CREATE TABLE `core_municipio` (
   PRIMARY KEY (`id`),
   KEY `core_municipio_estado_id_8d07bbda_fk_core_estado_id` (`estado_id`),
   CONSTRAINT `core_municipio_estado_id_8d07bbda_fk_core_estado_id` FOREIGN KEY (`estado_id`) REFERENCES `core_estado` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -277,6 +270,7 @@ CREATE TABLE `core_municipio` (
 
 LOCK TABLES `core_municipio` WRITE;
 /*!40000 ALTER TABLE `core_municipio` DISABLE KEYS */;
+INSERT INTO `core_municipio` VALUES (1,'Água Clara','2016-07-21 10:14:10','2016-07-21 10:14:10',11),(2,'Alcinópolis','2016-07-21 10:18:06','2016-07-21 10:18:06',11),(3,'Amambai','2016-07-21 10:18:18','2016-07-21 10:18:18',11),(4,'Anastácio','2016-07-21 10:18:26','2016-07-21 10:18:26',11),(5,'Anaurilândia','2016-07-21 10:18:36','2016-07-21 10:18:36',11),(6,'Angélica','2016-07-21 10:18:43','2016-07-21 10:18:43',11),(7,'Antônio João','2016-07-21 10:18:54','2016-07-21 10:18:54',11),(8,'Aparecida do Taboado','2016-07-21 10:19:09','2016-07-21 10:19:09',11),(9,'Aquidauana','2016-07-21 10:19:21','2016-07-21 10:19:21',11),(10,'Aral Moreira','2016-07-21 10:19:29','2016-07-21 10:19:29',11),(11,'Bandeirantes','2016-07-21 10:19:38','2016-07-21 10:19:38',11),(12,'Bataguassu','2016-07-21 10:19:49','2016-07-21 10:19:49',11),(13,'Batayporã','2016-07-21 10:20:01','2016-07-21 10:20:01',11),(14,'Bela Vista','2016-07-21 10:20:10','2016-07-21 10:20:10',11),(15,'Bodoquena','2016-07-21 10:20:44','2016-07-21 10:20:44',11),(16,'Bonito','2016-07-21 10:20:50','2016-07-21 10:20:50',11),(17,'Brasilândia','2016-07-21 10:20:59','2016-07-21 10:20:59',11),(18,'Caarapó','2016-07-21 10:21:07','2016-07-21 10:21:07',11),(19,'Camapuã','2016-07-21 10:21:16','2016-07-21 10:21:16',11),(20,'Campo Grande','2016-07-21 10:21:25','2016-07-21 10:21:25',11),(21,'Caracol','2016-07-21 10:21:32','2016-07-21 10:21:32',11),(22,'Cassilândia','2016-07-21 10:21:43','2016-07-21 10:21:43',11),(23,'Chapadão do Sul','2016-07-21 10:21:53','2016-07-21 10:21:53',11),(24,'Corguinho','2016-07-21 10:22:02','2016-07-21 10:22:02',11),(25,'Coronel Sapucaia','2016-07-21 10:22:10','2016-07-21 10:22:10',11),(26,'Corumbá','2016-07-21 10:22:16','2016-07-21 10:22:16',11),(27,'Costa Rica','2016-07-21 10:22:22','2016-07-21 10:22:22',11),(28,'Coxim','2016-07-21 10:22:27','2016-07-21 10:22:27',11),(29,'Deodápolis','2016-07-21 10:22:36','2016-07-21 10:22:36',11),(30,'Dois Irmãos do Buriti','2016-07-21 10:22:53','2016-07-21 10:22:53',11),(31,'Douradina','2016-07-21 10:23:12','2016-07-21 10:23:12',11),(32,'Dourados','2016-07-21 10:23:19','2016-07-21 10:23:19',11),(33,'Eldorado','2016-07-21 10:23:26','2016-07-21 10:23:26',11),(34,'Fátima do Sul','2016-07-21 10:23:34','2016-07-21 10:23:34',11),(35,'Figueirão','2016-07-21 10:23:41','2016-07-21 10:23:41',11),(36,'Glória de Dourados','2016-07-21 10:23:51','2016-07-21 10:23:51',11),(37,'Guia Lopes da Laguna','2016-07-21 10:24:02','2016-07-21 10:24:02',11),(38,'Iguatemi','2016-07-21 10:24:10','2016-07-21 10:24:10',11),(39,'Inocência','2016-07-21 10:24:16','2016-07-21 10:24:16',11),(40,'Itaporã','2016-07-21 10:24:25','2016-07-21 10:24:25',11),(41,'Itaquiraí','2016-07-21 10:24:39','2016-07-21 10:24:39',11),(42,'Ivinhema','2016-07-21 10:24:50','2016-07-21 10:24:50',11),(43,'Japorã','2016-07-21 10:24:59','2016-07-21 10:24:59',11),(44,'Jaraguari','2016-07-21 10:25:07','2016-07-21 10:25:07',11),(45,'Jardim','2016-07-21 10:25:11','2016-07-21 10:25:11',11),(46,'Jateí','2016-07-21 10:25:21','2016-07-21 10:25:21',11),(47,'Juti','2016-07-21 10:25:27','2016-07-21 10:25:27',11),(48,'Ladário','2016-07-21 10:25:35','2016-07-21 10:25:35',11),(49,'Laguna Carapã','2016-07-21 10:25:42','2016-07-21 10:25:42',11),(50,'Maracaju','2016-07-21 10:25:50','2016-07-21 10:25:50',11),(51,'Miranda','2016-07-21 10:25:55','2016-07-21 10:25:55',11),(52,'Mundo Novo','2016-07-21 10:26:01','2016-07-21 10:26:01',11),(53,'Naviraí','2016-07-21 10:26:10','2016-07-21 10:26:10',11),(54,'Nioaque','2016-07-21 10:26:20','2016-07-21 10:26:20',11),(55,'Nova Alvorada do Sul','2016-07-21 10:26:48','2016-07-21 10:26:48',11),(56,'Nova Andradina','2016-07-21 10:26:56','2016-07-21 10:26:56',11),(57,'Novo Horizonte do Sul','2016-07-21 10:27:06','2016-07-21 10:27:06',11),(58,'Paraíso das Águias','2016-07-21 10:27:20','2016-07-21 10:27:20',11),(59,'Paranaíba','2016-07-21 10:27:34','2016-07-21 10:27:34',11),(60,'Paranhos','2016-07-21 10:27:38','2016-07-21 10:27:38',11),(61,'Pedro Gomes','2016-07-21 10:27:44','2016-07-21 10:27:44',11),(62,'Ponta Porã','2016-07-21 10:27:52','2016-07-21 10:27:52',11),(63,'Porto Murtinho','2016-07-21 10:28:00','2016-07-21 10:28:00',11),(64,'Ribas do Rio Pardo','2016-07-21 10:28:10','2016-07-21 10:28:10',11),(65,'Rio Brilhante','2016-07-21 10:28:27','2016-07-21 10:28:27',11),(66,'Rio Negro','2016-07-21 10:28:34','2016-07-21 10:28:34',11),(67,'Rio Verde de Mato Grosso','2016-07-21 10:28:44','2016-07-21 10:28:44',11),(68,'Rochedo','2016-07-21 10:28:51','2016-07-21 10:28:51',11),(69,'Santa Rita do Pardo','2016-07-21 10:29:04','2016-07-21 10:29:04',11),(70,'São Gabriel do Oeste','2016-07-21 10:29:14','2016-07-21 10:29:14',11),(71,'Selviria','2016-07-21 10:29:23','2016-07-21 10:29:23',11),(72,'Sete Quedas','2016-07-21 10:29:29','2016-07-21 10:29:29',11),(73,'Sidrolândia','2016-07-21 10:29:37','2016-07-21 10:29:37',11),(74,'Sonora','2016-07-21 10:29:44','2016-07-21 10:29:44',11),(75,'Tacuru','2016-07-21 10:29:50','2016-07-21 10:29:50',11),(76,'Taquarussu','2016-07-21 10:29:58','2016-07-21 10:29:58',11),(77,'Terenos','2016-07-21 10:30:06','2016-07-21 10:30:06',11),(78,'Três Lagoas','2016-07-21 10:30:12','2016-07-21 10:30:12',11),(79,'Vicentina','2016-07-21 10:30:18','2016-07-21 10:30:18',11);
 /*!40000 ALTER TABLE `core_municipio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -301,7 +295,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
   CONSTRAINT `django_admin__content_type_id_c4bce8eb_fk_django_content_type_id` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -310,7 +304,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
-INSERT INTO `django_admin_log` VALUES (1,'2016-07-20 16:31:33','2','usuario',1,'Adicionado.',4,1),(2,'2016-07-20 16:31:56','3','master',1,'Adicionado.',4,1),(3,'2016-07-20 16:32:30','3','master',2,'Modificado user_permissions.',4,1),(4,'2016-07-20 16:33:31','3','master',2,'Modificado first_name, last_name e email.',4,1),(5,'2016-07-20 16:33:55','2','usuario',2,'Modificado first_name, last_name e email.',4,1);
+INSERT INTO `django_admin_log` VALUES (1,'2016-07-20 16:31:33','2','usuario',1,'Adicionado.',4,1),(2,'2016-07-20 16:31:56','3','master',1,'Adicionado.',4,1),(3,'2016-07-20 16:32:30','3','master',2,'Modificado user_permissions.',4,1),(4,'2016-07-20 16:33:31','3','master',2,'Modificado first_name, last_name e email.',4,1),(5,'2016-07-20 16:33:55','2','usuario',2,'Modificado first_name, last_name e email.',4,1),(6,'2016-07-21 09:37:58','1','admin',2,'Modificado first_name e last_name.',4,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -388,6 +382,7 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
+INSERT INTO `django_session` VALUES ('whksw2pz86nulljpmkm33kngfx1y5r0z','ODE2MzMxMzQwMTIwYjc0ZWU1NjBmMGVjMjVhOWUyMDY3MzUwNzc1YTp7Il9hdXRoX3VzZXJfaWQiOiIzIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiI3YjU1MjZiNGFmZWE0MmYyMmIxYmQwYTU0NmVmOTI2Yjc5MmIyYzk3In0=','2016-08-04 10:35:37');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -423,7 +418,7 @@ CREATE TABLE `fornecedor_fornecedor` (
   CONSTRAINT `fornecedor_fornecedor_cidade_id_f55ba7fa_fk_core_municipio_id` FOREIGN KEY (`cidade_id`) REFERENCES `core_municipio` (`id`),
   CONSTRAINT `fornecedor_fornecedor_estado_id_cc29b1e6_fk_core_estado_id` FOREIGN KEY (`estado_id`) REFERENCES `core_estado` (`id`),
   CONSTRAINT `fornecedor_fornecedor_usuario_id_0c6232f8_fk_auth_user_id` FOREIGN KEY (`usuario_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -432,6 +427,7 @@ CREATE TABLE `fornecedor_fornecedor` (
 
 LOCK TABLES `fornecedor_fornecedor` WRITE;
 /*!40000 ALTER TABLE `fornecedor_fornecedor` DISABLE KEYS */;
+INSERT INTO `fornecedor_fornecedor` VALUES (1,'ZILIOTTO INDÚSTRIA, ATACADO, COMERCIO E REPRESENTACOES LTDA','ZORNIMAT','15491434000109','Fabricação de móveis','Rua Quatorze de Julho','931','Loja','79004391','(67) 3025-7272','Marcio Rodrigues','2016-07-21 10:34:45','2016-07-21 10:35:47',20,11,1,3);
 /*!40000 ALTER TABLE `fornecedor_fornecedor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -473,4 +469,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-20 12:38:07
+-- Dump completed on 2016-07-21  6:37:02
