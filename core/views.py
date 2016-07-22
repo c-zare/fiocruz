170657864a9a0f90d788d2e06c9d5cd816b23ede
Julio Coimbra <jcoimbra@fiocruz.br>
2016-07-22 10:48:15 -0400

    Ajustes projto.txt

--- core/views.py
+++ core/views.py
@@ -19,3 +19,17 @@ def usuario_detalhe(request):
 #		return HttpResponseRedirect(instance.get_absolute_url())
 #	context = {'form':form,}
 #	return render(request,'armazem_form.html', context)	
+
+
+#@login_required
+#def fornecedor_edita(request,id=None):#
+#	instance = get_object_or_404(Fornecedor,id=id)
+#	form = FornecedorForm(request.POST or None, instance=instance)
+#	if form.is_valid():
+#		instance = form.save(commit=False)
+#		instance.usuario = request.user
+#		instance.save()
+#		messages.success(request,' Fornecedor foi modificado.')
+#		return HttpResponseRedirect(instance.get_absolute_url())
+#	context = { 'instance':instance, 'form':form }
+#	return render(request,'fornecedor_form.html', context)
