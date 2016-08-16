8a2bf4b1795fa54eb3a2c253cb156118ca4c0298
Julio Coimbra <jcoimbra@fiocruz.br>
2016-08-16 15:15:19 -0300

    Pano de Fundo

--- fornecedor/forms.py
+++ fornecedor/forms.py
@@ -8,7 +8,7 @@ class FornecedorForm(forms.ModelForm):
 
 #	telefone	= forms.RegexField(regex=r'^\+?1?\d{9,15}$',error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
 	situacao 	= forms.BooleanField(required=False, label="situacao")
-#	cidade 		= forms.ModelChoiceField(queryset=Municipio.objects.all().filter(estado_id=11))
+	cidade 		= forms.ModelChoiceField(queryset=Municipio.objects.all().filter(estado_id=11))
 
 	class Meta:
 		model = Fornecedor
