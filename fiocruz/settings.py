787a8a5ab4cbc780dc8be50c6be77df1fe1b5205
Julio Coimbra <jcoimbra@fiocruz.br>
2016-08-18 15:34:35 -0300

    Limpeza das migrações para implementação de compras

--- fiocruz/settings.py
+++ fiocruz/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'core',
     'fornecedor',
     'item',
+    'compra',
  ]
 
 MIDDLEWARE_CLASSES = [
--- fiocruz/urls.py
+++ fiocruz/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.urls import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -7,6 +8,6 @@ urlpatterns = [
     url(r'^fornecedor/', include('fornecedor.urls', namespace='fornecedor')),
     url(r'^item/', include('item.urls', namespace='item')),
     url(r'^usuario/', include('core.urls', namespace='core')),
-    url(r'^login/','django.contrib.auth.views.login', {'template_name':'login.html'}, name='login'),
-    url(r'^logout/','django.contrib.auth.views.logout_then_login', { 'login_url': '/login/'}, name='logout')
+    url(r'^login/',views.login, {'template_name':'login.html'}, name='login'),
+    url(r'^logout/',views.logout_then_login, { 'login_url': '/login/'}, name='logout')
 ]
